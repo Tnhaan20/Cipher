459cdacb48194ee1524a2d60e2cc1732db5627d9
DavidN2076 <nhancd909@gmail.com>
2024-09-30 15:21:04 +0700

    done: upload photo to album

--- src/views/Photos/Photos.tsx
+++ src/views/Photos/Photos.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import './Photos.css'
@@ -12,13 +12,22 @@ interface Photo {
 }
 
 export default function AlbumPhotos() {
+  const botToken = import.meta.env.VITE_BOT_TOKEN
+  const chatId = import.meta.env.VITE_CHAT_ID
   const { id } = useParams<{ id: string }>();
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [modal, setModalOpen] = useState(false)
   const [currentImg, setCurrentImg] = useState(0)
-
+  const [uploadModal, setUploadOpen] = useState(false)
+  const [dragActive, setDragActive] = useState(false);
+  const [uploadedFile, setUploadedFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [titleModal, setTitleModal] = useState(false)
+  const [newPhoto, setNewPhoto] = useState({title: ""});
+  const [fileID, setFileID] = useState("")
+  const [url, setUrl] = useState("")
   const loadPhotos = async () => {
     try {
       setLoading(true);
@@ -34,12 +43,18 @@ export default function AlbumPhotos() {
       setLoading(false);
     }
   };
-  
-  
 
   useEffect(() => {
     loadPhotos();
   }, [id]);
+
+  const open = () => {
+    setUploadOpen(true)
+  }
+
+  const close = () => {
+    setUploadOpen(false)
+  }
 
   const openModal = (index: number) => {
     setCurrentImg(index);
@@ -58,13 +73,132 @@ export default function AlbumPhotos() {
     setCurrentImg((next)=> (next+1)% photos.length)
   }
 
+  const handleDrag = useCallback((e: React.DragEvent<HTMLFormElement | HTMLLabelElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  }, []);
 
+  const handleDrop = useCallback((e: React.DragEvent<HTMLFormElement | HTMLLabelElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      handleFiles(e.dataTransfer.files[0]);
+    }
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      handleFiles(e.target.files[0]);
+    }
+  };
+
+  const handleFiles = async (file: File) => {
+    if (file.type.startsWith('image/')) {
+      setUploadedFile(file);
+      console.log(file);
+      console.log('aaaa');
+      const fileUrl = URL.createObjectURL(file);
+      setPreviewUrl(fileUrl);
+    } else {
+      alert('Please upload an image file.');
+    }
+  };
+
+  
+  const handleNext = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (uploadedFile) {
+      try {
+        const fileId = await uploadFileToCDN();
+        const secureUrl = await getImgFromCDN(fileId);
+        setUrl(secureUrl);
+        setUploadOpen(false);
+        setTitleModal(true);
+      } catch (error) {
+        console.error('Error during file upload:', error);
+        alert('Failed to upload the file. Please try again.');
+      }
+    }
+  };
+
+  const handleTitleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (uploadedFile && newPhoto.title.trim() && url) {
+      try {
+        await uploadImgToAPI();
+        setTitleModal(false);
+        setUploadedFile(null);
+        setPreviewUrl(null);
+        setNewPhoto({title: ""});
+        setUrl("");
+        loadPhotos(); // Reload photos after successful upload
+      } catch (error) {
+        console.error('Error uploading photo:', error);
+        alert('Failed to upload the photo. Please try again.');
+      }
+    } else {
+      alert('Please enter a valid title for the image and ensure the image URL is available.');
+    }
+  };
+
+  const uploadFileToCDN = async (): Promise<string> => {
+    if (!uploadedFile) {
+      throw new Error('No file selected');
+    }
+    try {
+      const formData = new FormData();
+      formData.append('bot_token', botToken);
+      formData.append('chat_id', chatId);
+      formData.append('document', uploadedFile);
+      const response = await axios.post('https://cdn.thinology.id.vn/send', formData);
+      console.log('File uploaded successfully:', response.data);
+      return response.data.data.id;
+    } catch (error) {
+      console.error('Error uploading to CDN:', error);
+      throw error;
+    }
+  }
+  
+  const getImgFromCDN = async (fileId: string): Promise<string> => {
+    try {
+      const response = await axios.get(`https://cdn.thinology.id.vn/url?bot_token=${botToken}&file_id=${fileId}`);
+      console.log('File URL retrieved successfully:', response.data);
+      return response.data.data.secure_url;
+    } catch (error) {
+      console.error('Error getting image URL from CDN:', error);
+      throw error;
+    }
+  }
+
+  const uploadImgToAPI = async () => {
+    try {
+      const response = await axios.post('https://jsonplaceholder.typicode.com/photos', {
+        albumId: id,
+        title: newPhoto.title,
+        url: url,
+        thumbnailUrl: url,
+      });
+      console.log('Photo posted successfully:', response.data);
+    } catch (error) {
+      console.error('Error uploading photo to API:', error);
+      throw error;
+    }
+  };
 
   return (
     <div className="w-full text-white pt-28 px-4 min-h-screen">
-      
       <div className="w-full flex justify-center p-5">
         <h1 className="text-3xl font-bold">Album {id}</h1>
+        <button className='button-search' onClick={open}>
+          Upload photo
+        </button>
       </div>
 
       {error && <p className="text-red-500 text-center my-4">{error}</p>}
@@ -120,6 +254,86 @@ export default function AlbumPhotos() {
           </div>
         </div>
       )}
+
+      {uploadModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+          <div className="bg-white rounded-lg p-8 max-w-md w-full relative">
+            <button 
+              onClick={() => setUploadOpen(false)} 
+              className="absolute top-2 right-2 text-black text-xl"
+            >
+              &times;
+            </button>
+            <form className="form" onSubmit={handleNext} onDragEnter={handleDrag}>
+              <span className="form-title">Upload your file</span>
+              <p className="form-paragraph">
+                File should be an image
+              </p>
+              <label 
+                htmlFor="file-input" 
+                className={`drop-container ${dragActive ? 'drag-active' : ''}`}
+                onDragEnter={handleDrag}
+                onDragLeave={handleDrag}
+                onDragOver={handleDrag}
+                onDrop={handleDrop}
+              >
+                <span className="drop-title">Drop files here</span>
+                or
+                <input 
+                  type="file" 
+                  accept="image/*" 
+                  required 
+                  id="file-input"
+                  onChange={handleChange}
+                />
+              </label>
+              {previewUrl && (
+                <div className="preview-container">
+                  <img src={previewUrl} alt="Preview" className="preview-image" />
+                </div>
+              )}
+              {uploadedFile && (
+                <button type="submit" className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full">
+                  Next
+                </button>
+              )}
+
+            </form>
+          </div>
+        </div>
+      )}
+
+{titleModal && (
+      <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+        <div className="bg-white rounded-lg p-8 max-w-md w-full relative">
+          <button 
+            onClick={() => setTitleModal(false)} 
+            className="absolute top-2 right-2 text-black text-xl"
+          >
+            &times;
+          </button>
+          <form onSubmit={handleTitleSubmit} className="flex flex-col items-center">
+            <h2 className="text-2xl font-bold mb-4 text-black">Add a title</h2>
+            {previewUrl && (
+              <div className="preview-container mb-4">
+                <img src={previewUrl} alt="Preview" className="preview-image"/>
+              </div>
+            )}
+            <input
+              type="text"
+              value={newPhoto.title}
+              onChange={(e) => setNewPhoto({...newPhoto, title: e.target.value})}
+              placeholder="Enter image title"
+              className="w-full p-2 mb-4 border border-gray-300 rounded text-black"
+              required
+            />
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full">
+              Upload
+            </button>
+          </form>
+        </div>
+      </div>
+    )}
     </div>
   );
 }
